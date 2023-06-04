3dc32f9f3f0c28533d15160bece3f05166085261
unknown <git@github.com:Jorgepaz0s/TechCamus.git>
2023-06-04 17:36:59 -0300

    inicio responsive 1.2

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 });
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
+
+body.addEventListener('click', (event) => {
+  const target = event.target;
+  
+  // Comprueba si el clic se realizó fuera del menú hamburguesa
+  if (!target.closest('header')) {
+    nav.classList.remove('nav-active');
+  }
+});
